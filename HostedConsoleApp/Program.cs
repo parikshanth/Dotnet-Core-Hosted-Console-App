536ada2aec80c34a58f5b4abf2eac535429b96bd
Parikshanth <parikshanth@hotmail.com>
2021-06-13 14:13:51 +0530

    Add project files.

---
+++ HostedConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace HostedConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .Enrich.FromLogContext()
+                .WriteTo.Console(outputTemplate: "{Timestamp:G} [{Level:u3}] [{SourceContext}] {Message} {NewLine:1}{Exception:1}")
+                .CreateLogger();
+
+            await new HostBuilder().ConfigureServices((hostContext, service) =>
+            {
+                service.AddScoped<IHostedService, TestService>();
+            }).UseSerilog().RunConsoleAsync();
+
+
+
+        }
+    }
+}
---
+++ HostedConsoleApp/TestService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedConsoleApp
+{
+    internal class TestService : IHostedService
+    {
+        private readonly ILogger<TestService> _logger;
+        private bool looper = false;
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+            looper = true;
+            _logger.LogInformation("Started Test Service");
+
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(() =>
+            {
+                while (looper)
+                {
+                    _logger.LogInformation("Test Service Running");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            looper = false;
+            _logger.LogInformation("Stopping Test Service");
+
+            return Task.CompletedTask;
+        }
+    }
+}
